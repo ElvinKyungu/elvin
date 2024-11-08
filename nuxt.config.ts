bc993c7063a4817dc02f7697e4c55f96667a7c88
Elvin Kyungu <elvinkyungu.75@gmail.com>
2024-11-08 19:56:00 +0200

    add i18n module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['@/assets/css/main.css'],
-  modules: ['@vueuse/nuxt', '@nuxtjs/tailwindcss', '@nuxtjs/i18n']
+  modules: ['@vueuse/nuxt', '@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
+  i18n: {
+    locales: [
+      { code: 'en', language: 'en-US' },
+      { code: 'fr', language: 'fr-FR' }
+    ],
+    defaultLocale: 'en',
+  }
 })
