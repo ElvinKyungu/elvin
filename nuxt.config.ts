e8fa3f48d5be065b456fb4414e35c11028cca044
Elvin Kyungu <elvinkyungu.75@gmail.com>
2024-11-11 21:00:23 +0200

    config i18n and added some text in english for home page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,8 +5,23 @@ export default defineNuxtConfig({
   css: ['@/assets/css/main.css'],
   modules: ['@vueuse/nuxt', '@nuxtjs/tailwindcss', '@nuxtjs/i18n'],
   i18n: {
-    vueI18n: './i18n.config.ts',
-    locales: ['en', 'fr'], // used in URL path prefix
-    defaultLocale: 'en', // default locale of my project for Nuxt pages and routings
+    lazy: true,
+    langDir: 'locales',
+    strategy: 'prefix_except_default',
+    locales: [
+      {
+        code: "en",
+        iso: "en-US",
+        name: "English",
+        file: "en-US.json"
+      },
+      {
+        code: "fr",
+        iso: "fr-FR",
+        name: "Français",
+        file: "fr-France.json"
+      }
+    ],
+    defaultLocale: "en",
   }
 })
