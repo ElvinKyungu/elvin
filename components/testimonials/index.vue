8f711aa557ebc547c59f7f432a7023a55da74379
Elvin Kyungu <elvinkyungu.75@gmail.com>
2024-11-25 23:36:01 +0200

    resolve error in Testimonial component (index.vue) to display data

--- components/testimonials/index.vue
+++ components/testimonials/index.vue
@@ -2,6 +2,11 @@
 import { useParagraphAnimation } from '@/composables/useParagraphAnimation'
 
 
+const { rt, t, tm, messages } = useI18n()
+
+const testimonials = tm("home_testimonials")
+console.log(testimonials)
+
 const paragraphRef = ref<HTMLElement | null>(null)
 useParagraphAnimation(paragraphRef, 0.5)
 </script>
@@ -11,17 +16,16 @@ useParagraphAnimation(paragraphRef, 0.5)
     <div
       class="absolute z-0 inset-0 h-full w-full bg-[radial-gradient(theme(colors.green.900)_1px,transparent_1px)] [background-size:16px_16px]"
     ></div>
-    <!-- <span class="text-sm md:text-sm">{{ $t('second_wallet_btn') }}</span> -->
-    <h2 class="text-3xl lg:text-5xl relative mb-10">{{ $t('home_title_testimonial') }}</h2>
+    <h2 class="text-3xl lg:text-5xl relative mb-10">{{ t('home_title_testimonial') }}</h2>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 relative">
       <TestimonialsTestimonialCard
-        v-for="(testimonial, index) in $t('home_testimonials')"
+        v-for="(testimonial, index) in testimonials"
         :key="index"
         class="testimonial-card"
-        :user-image="testimonial.image"
-        :user-text="testimonial.message"
-        :user-name="testimonial.name"
-        :user-title="testimonial.title"
+        :user-image="rt(testimonial.image)"
+        :user-text="rt(testimonial.message)"
+        :user-name="rt(testimonial.name)"
+        :user-title="rt(testimonial.title)"
       />
     </div>
     <div>
